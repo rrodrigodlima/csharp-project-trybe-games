b5ed1668724b75d10501de6dff5094c49e6ffef4
rrodrigodlima <rdrigo019@gmail.com>
2023-10-03 12:37:32 -0300

    req05

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -21,7 +21,9 @@ public class TrybeGamesDatabase
     public List<Game> GetGamesPlayedBy(Player player)
     {
         // Implementar
-        throw new NotImplementedException();
+        var gamesPlayedByPlayer = Games.Where(game => player.GamesOwned.Contains(game.Id)).ToList();
+
+        return gamesPlayedByPlayer;
     }
 
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
